6abcb6eb01573a986e9e8f3a736b12d240a05433
Brian Haberer <bhaberer@gmail.com>
2014-08-02 15:00:01 -0700

    v1.0.4 - spec refactor

--- lib/cinch/plugins/urbandict.rb
+++ lib/cinch/plugins/urbandict.rb
@@ -3,52 +3,54 @@ require 'cinch'
 require 'cinch/cooldown'
 require 'cinch/toolbox'
 
-module Cinch::Plugins
-  # Cinch plugin for getting UD defs.
-  class UrbanDict
-    include Cinch::Plugin
+module Cinch
+  module Plugins
+    # Cinch plugin for getting UD defs.
+    class UrbanDict
+      include Cinch::Plugin
 
-    enforce_cooldown
+      enforce_cooldown
 
-    self.help = 'Use .ud <term> to see the Urban Dictionary definition.'
+      self.help = 'Use .ud <term> to see the Urban Dictionary definition.'
 
-    match /ud (.*)/
+      match(/ud (.*)/)
 
-    def execute(m, term)
-      m.reply get_def(term), true
-    end
-
-    private
-
-    def get_def(term)
-      # URI Encode
-      term = URI.escape(term, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
-
-      # Check if it's not defined.
-      if Cinch::Toolbox.get_html_element(ud_url(term), '#not_defined_yet')
-        return "Urban Dictionary ∴ #{term}: No definition available."
+      def execute(m, term)
+        m.reply get_def(term), true
       end
 
-      # Get the def
-      ud_def(term)
-    end
+      private
 
-    def ud_def(term)
-      # Grab the element
-      result = Cinch::Toolbox.get_html_element(ud_url(term), '.meaning')
+      def get_def(term)
+        # URI Encode
+        term = URI.escape(term, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
 
-      result.gsub!(/[\n\r]/, ' - ')
+        # Check if it's not defined.
+        if Cinch::Toolbox.get_html_element(ud_url(term), '#not_defined_yet')
+          return "Urban Dictionary ∴ #{term}: No definition available."
+        end
 
-      # Make sure it's not terribly long
-      result = Cinch::Toolbox.truncate(result, 250)
-      url = Cinch::Toolbox.shorten(ud_url(term))
+        # Get the def
+        ud_def(term)
+      end
 
-      debug result
-      "Urban Dictionary ∴ #{term}: #{result} [#{url}]"
-    end
+      def ud_def(term)
+        # Grab the element
+        result = Cinch::Toolbox.get_html_element(ud_url(term), '.meaning')
 
-    def ud_url(term)
-      "http://www.urbandictionary.com/define.php?term=#{term}"
+        result.gsub!(/[\n\r]/, ' - ')
+
+        # Make sure it's not terribly long
+        result = Cinch::Toolbox.truncate(result, 250)
+        url = Cinch::Toolbox.shorten(ud_url(term))
+
+        debug result
+        "Urban Dictionary ∴ #{term}: #{result} [#{url}]"
+      end
+
+      def ud_url(term)
+        "http://www.urbandictionary.com/define.php?term=#{term}"
+      end
     end
   end
 end
--- spec/cinch-urbandict_spec.rb
+++ spec/cinch-urbandict_spec.rb
@@ -9,24 +9,25 @@ describe Cinch::Plugins::UrbanDict do
   end
 
   it 'should allow users to search for words' do
-    get_replies(make_message(@bot, '!ud dork')).first.text.
-      should match('Someone who has odd interests')
+    msg = get_replies(make_message(@bot, '!ud dork')).first
+    expect(msg.text).to match('Someone who has odd interests')
   end
 
   it 'should not return nil for valid searches' do
-    get_replies(make_message(@bot, '!ud dork')).first.text.
-      should_not be_nil
+    msg = get_replies(make_message(@bot, '!ud dork')).first
+    expect(msg.text).to_not be_nil
   end
 
   it 'should return an error for invalid searches' do
-    get_replies(make_message(@bot, '!ud 7zJ6t8rT8p384My')).first.text.
-      should == "test: Urban Dictionary ∴ 7zJ6t8rT8p384My: No definition available."
+    msg = get_replies(make_message(@bot, '!ud 7zJ6t8rT8p384My')).first
+    expect(msg.text)
+    .to match('test: Urban Dictionary ∴ 7zJ6t8rT8p384My: No definition available.')
   end
 
   it 'should not return multiple lines for a definition with breaks' do
     response = get_replies(make_message(@bot, '!ud dong')).first.text
-    response.should_not match("<br")
-    response.should_not match("\r")
-    response.should_not match("\n")
+    expect(response).to_not match('<br')
+    expect(response).to_not match('\r')
+    expect(response).to_not match('\n')
   end
 end
